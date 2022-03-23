305431e289b5d620291eb92f445f0ffb129386a8
ayeAlvo <ayealvo@gmail.com>
2022-03-22 23:42:05 -0300

    config heroku mysql

--- website/database/config/config.js
+++ website/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "calamarket_db",
-    "host": "127.0.0.1",
+    "username": "b80ee16c8cb802",
+    "password": "cad627b8",
+    "database": "heroku_b95869c99c1c310",
+    "host": "us-cdbr-east-05.cleardb.net",
     "dialect": "mysql"
   },
   "test": {
