97824a9ba1e97f19d03ae0560bf56394e461d124
ayeAlvo <ayealvo@gmail.com>
2022-03-23 00:08:12 -0300

    cambios en la vista

--- website/database/config/config.js
+++ website/database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "b80ee16c8cb802",
+    "password": "cad627b8",
+    "database": "heroku_b95869c99c1c310",
+    "host": "us-cdbr-east-05.cleardb.net",
     "dialect": "mysql"
   }
 }
